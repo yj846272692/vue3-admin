305b6e9fc18efb8c917a27ceefbff51761bcc077
yj846272692 <846272692@qq.com>
2022-05-05 00:50:17 +0800

    feat: 自定义语言包国际化处理

---
+++ src/i18n/lang/index.js
@@ -0,0 +1,14 @@
+import mZhLocale from './lang/zh'
+import mEnLocale from './lang/en'
+const messages = {
+  en: {
+    msg: {
+      ...mEnLocale
+    }
+  },
+  zh: {
+    msg: {
+      ...mZhLocale
+    }
+  }
+}
