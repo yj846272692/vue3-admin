1df53201195586276393ead27099ceb297e9c502
yj846272692 <846272692@qq.com>
2022-05-04 23:02:51 +0800

    feat: 获取用户基本信息

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
+import store from '@/store'
 
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
@@ -22,6 +23,21 @@ service.interceptors.response.use(
   (error) => {
     // TODO: 将来处理 token 超时问题
     ElMessage.error(error.message) // 提示错误信息
+    return Promise.reject(error)
+  }
+)
+
+// 请求拦截器
+service.interceptors.request.use(
+  (config) => {
+    // 在这个位置需要统一的去注入token
+    if (store.getters.token) {
+      // 如果token存在 注入token
+      config.headers.Authorization = `Bearer ${store.getters.token}`
+    }
+    return config // 必须返回配置
+  },
+  (error) => {
     return Promise.reject(error)
   }
 )
