c8ef95b41541c0e1f6b1d066a956d7d60af57da1
yj846272692 <846272692@qq.com>
2022-05-05 20:46:07 +0800

    feat: 检索数据源，路由表数据处理

--- src/components/HeaderSearch/index.vue
+++ src/components/HeaderSearch/index.vue
@@ -16,18 +16,24 @@
       :remote-method="querySearch"
       @change="onSelectChange"
     >
+      <!-- <el-option v-for="option in 5" :key="option" :label="option" :value="option"></el-option> -->
       <el-option
-        v-for="option in 5"
-        :key="option"
-        :label="option"
-        :value="option"
+        v-for="option in searchOptions"
+        :key="option.item.path"
+        :label="option.item.title.join(' > ')"
+        :value="option.item"
       ></el-option>
     </el-select>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { computed, ref, watch } from 'vue'
+import { filterRouters } from '@/utils/route'
+import { useRouter } from 'vue-router'
+import { generateRoutes } from './FuseData'
+import { watchSwitchLang } from '@/utils/i18n'
+import Fuse from 'fuse.js'
 
 // 控制 search 显示
 const isShow = ref(false)
@@ -37,17 +43,82 @@ const onShowClick = () => {
   isShow.value = !isShow.value
   headerSearchSelectRef.value.focus()
 }
+// 搜索结果
+const searchOptions = ref([])
 
 // search 相关
 const search = ref('')
 // 搜索方法
-const querySearch = () => {
-  console.log('querySearch')
+const querySearch = (query) => {
+  // console.log(fuse.search(query))
+  if (query !== '') {
+    searchOptions.value = fuse.search(query)
+  } else {
+    searchOptions.value = []
+  }
 }
 // 选中回调
-const onSelectChange = () => {
-  console.log('onSelectChange')
+const onSelectChange = (val) => {
+  router.push(val.path)
+  onClose()
 }
+
+const onClose = () => {
+  headerSearchSelectRef.value.blur()
+  isShow.value = false
+  searchOptions.value = []
+}
+watch(isShow, (val) => {
+  if (val) {
+    document.body.addEventListener('click', onClose)
+  } else {
+    document.body.removeEventListener('click', onClose)
+  }
+})
+
+// 检索数据源
+const router = useRouter()
+let searchPool = computed(() => {
+  const filterRoutes = filterRouters(router.getRoutes())
+  return generateRoutes(filterRoutes)
+})
+
+/**
+ * 搜索库相关
+ */
+
+let fuse
+const initFuse = (searchPool) => {
+  fuse = new Fuse(searchPool.value, {
+    // 是否按优先级进行排序
+    shouldSort: true,
+    // 匹配长度超过这个值的才会被认为是匹配的
+    minMatchCharLength: 1,
+    // 将被搜索的键列表。 这支持嵌套路径、加权搜索、在字符串和对象数组中搜索。
+    // name：搜索的键
+    // weight：对应的权重
+    keys: [
+      {
+        name: 'title',
+        weight: 0.7
+      },
+      {
+        name: 'path',
+        weight: 0.3
+      }
+    ]
+  })
+}
+
+initFuse(searchPool)
+
+watchSwitchLang(() => {
+  searchPool = computed(() => {
+    const filterRoutes = filterRouters(router.getRoutes())
+    return generateRoutes(filterRoutes)
+  })
+  initFuse(searchPool)
+})
 </script>
 
 <style lang="scss" scoped>
