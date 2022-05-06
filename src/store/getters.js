5def7dfe8107055de7749d47ce5987535f761594
yj846272692 <846272692@qq.com>
2022-05-06 19:53:05 +0800

    feat: 生成 tagsView

--- src/store/getters.js
+++ src/store/getters.js
@@ -4,15 +4,15 @@ import { getItem } from '@/utils/storage'
 import { generateColors } from '@/utils/theme'
 
 const getters = {
-  token: state => state.user.token,
-  userInfo: state => state.user.userInfo,
+  token: (state) => state.user.token,
+  userInfo: (state) => state.user.userInfo,
   /**
    * @returns true 表示已存在用户信息
    */
-  hasUserInfo: state => {
+  hasUserInfo: (state) => {
     return JSON.stringify(state.user.userInfo) !== '{}'
   },
-  cssVar: state => {
+  cssVar: (state) => {
     return {
       ...state.theme.variables,
       ...generateColors(getItem(MAIN_COLOR))
@@ -20,6 +20,7 @@ const getters = {
   },
   sidebarOpened: (state) => state.app.sidebarOpened,
   language: (state) => state.app.language,
-  mainColor: state => state.theme.mainColor
+  mainColor: (state) => state.theme.mainColor,
+  tagsViewList: (state) => state.app.tagsViewList
 }
 export default getters
