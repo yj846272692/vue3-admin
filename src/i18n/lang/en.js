a5ea6e2ac04e3910310c5ef0c3afaef254475450
yj846272692 <846272692@qq.com>
2022-05-06 22:15:15 +0800

    feat: 作者模块开发

--- src/i18n/lang/en.js
+++ src/i18n/lang/en.js
@@ -90,10 +90,9 @@ export default {
     feature: 'Feature',
     chapter: 'Chapter',
     author: 'Author',
-    name: 'Sunday',
-    job: 'A front-end development program',
-    Introduction:
-      'A senior technical expert, once worked in a domestic first-line Internet company, and has coordinated multiple large-scale projects with more than tens of millions of users. Committed to researching big front-end technology, he has been invited to participate in domestic front-end technology sharing sessions many times, such as: Google China Technology Sharing Session in 2018.'
+    name: 'YG',
+    job: 'Student',
+    Introduction: 'A smoky rain and Ren Pingsheng'
   },
   userInfo: {
     print: 'Print',
