7cdeb3d8102015de785c3cec15c392e3fef60763
yj846272692 <846272692@qq.com>
2022-05-09 16:45:56 +0800

    feat: 定义页面权限，重置路由表数据

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,14 @@ import user from './modules/user'
 import app from './modules/app'
 import getters from './getters'
 import theme from './modules/theme'
+import permission from './modules/permission'
 
 export default createStore({
   getters,
   modules: {
     user,
     app,
-    theme
+    theme,
+    permission
   }
 })
